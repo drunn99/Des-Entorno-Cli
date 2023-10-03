2546c9a843d751a23ea3f24954fc4fc923c5c54b
drunn <drunn.99@gmail.com>
2023-10-03 22:22:38 +0200

    3-10.4

--- EjercicioCambioColor/scripts.js
+++ EjercicioCambioColor/scripts.js
@@ -1,8 +1,11 @@
-let r,g,b = 0;
 
 function newRandomColor (){
+    let actualColor =  document.getElementById("actualColor");
+    let r,g,b = 0;
     r = Math.floor(Math.random()*255);
     g = Math.floor(Math.random()*255);
     b = Math.floor(Math.random()*255);
     document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+    actualColor.innerHTML = `rgb(${r},${g},${b})`;
+
 }
