00da01034b36eca00bd96484989ff8751db67adf
Daniel Núñez <drunn.99@gmail.com>
2023-10-04 13:44:48 +0200

    4-10.4

--- EjercicioCambioColor/hex/index.js
+++ EjercicioCambioColor/hex/index.js
@@ -4,7 +4,7 @@ let btn = document.getElementById("btn");
 btn.addEventListener("click",newRandomColor);
 
 function newRandomColor(){
-    let colorText = document.getElementById("color");
+    let colorText = document.getElementsByClassName("color");
     let hexColor = "#";
     let selector = 0;
     for(let i = 0; i < 6; i++){
@@ -13,7 +13,7 @@ function newRandomColor(){
         hexColor += hex[selector];
     }
     document.body.style.backgroundColor = hexColor;
-    colorText.innerHTML = hexColor;
+    colorText[0].innerHTML = hexColor;
 
 }
 
